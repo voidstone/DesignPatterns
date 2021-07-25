874e1289637302f59c2b4dd1c18b0c62f22e276c
sage <chaikin.sergey@gmail.com>
2021-07-25 12:05:52 +0400

    feat: add interface pattern

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Auth::routes();
 Route::get('/property-container', [App\Http\Controllers\FundamentalPatternsController::class, 'PropertyContainer'])->name('property-container');
 Route::get('/delegation', [App\Http\Controllers\FundamentalPatternsController::class, 'delegation'])->name('delegation');
 Route::get('/eventChannel', [App\Http\Controllers\FundamentalPatternsController::class, 'eventChannel'])->name('eventChannel');
+Route::get('/interface', [App\Http\Controllers\FundamentalPatternsController::class, 'interface'])->name('interface');
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
